74b0d62b795382c7509a467095e003e16c629571
Marcelo Arias <hello@marceloarias.com>
2023-03-21 13:41:36 -0500

    Add How it works page

---
+++ src/pages/3_⚡️_How_it_works.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("⚡️ How it works")
+
+st.write("## Concept")
+
+st.write("The idea behind *Generative Manim* is test how far we can go with GPT-3.5. In 2020, (Shreenabh Agrawal)[https://twitter.com/ShreenabhA] did the same with the previous GPT-3.")
+
