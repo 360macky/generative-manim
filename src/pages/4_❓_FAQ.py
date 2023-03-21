1795ed2494842fc43aa50c13ccd70e26b6ccc254
Marcelo Arias <hello@marceloarias.com>
2023-03-21 12:35:56 -0500

    Update questions

---
+++ src/pages/4_❓_FAQ.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("❓ FAQ")
+
+st.write("## What is Manim?")
+
+st.write("Manim is a Python library for creating mathematical animations. It's open source. The version used in this app is Manim Community, a fork of the original Manim library.")
+
+st.write("## What is GPT-3.5?")
+
+st.write("GPT-3.5 is the latest API-available version of OpenAI's GPT model. It's a language model behind ChatGPT. GPT-4 itself is not available as an API.")
+
