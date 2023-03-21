9f24eb74832aab9d9bfe66ac066ba37e334b1084
Marcelo Arias <hello@marceloarias.com>
2023-03-21 11:11:42 -0500

    Rename page to Render Engine

---
+++ src/pages/1_💻_Render_Engine.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown('# 💻 Render Engine')
+
+st.write("Quick engine to render Manim code. Paste your code, and click generate. That's it!")
--- src/pages/page_1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.markdown('# Hello world!')
-st.sidebar.markdown('# Hello world!')
-st.write("Hello world")
