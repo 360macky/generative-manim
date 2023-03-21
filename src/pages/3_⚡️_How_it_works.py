7e4be631192166bd1fab158d8ea5d690dd58b893
Marcelo Arias <hello@marceloarias.com>
2023-03-21 14:31:58 -0500

    Add How It Works article

--- src/pages/3_⚡️_How_it_works.py
+++ src/pages/3_⚡️_How_it_works.py
@@ -4,5 +4,24 @@ st.title("⚡️ How it works")
 
 st.write("## Concept")
 
-st.write("The idea behind *Generative Manim* is test how far we can go with GPT-3.5. In 2020, (Shreenabh Agrawal)[https://twitter.com/ShreenabhA] did the same with the previous GPT-3.")
+st.markdown("""
 
+Imagine a future where you can watch an animation video from a concept you have in your mind in a few seconds. That could be useful for teachers, students, and more. You won't need to learn how to use a video editor, how to draw, or how to code. You just need to write a text.
+
+That indeed is a future. But it's not that far. We can already start working on it. I have a feel that a pure AI editor like [Runway](https://runwayml.com), or an animation editor like [Jitter](https://jitter.video) could take advantage of a workflow like this.
+
+Storytelling, teaching, and creating it's about to change.
+
+
+
+
+The idea behind *Generative Manim* is test how far we can go with GPT-3.5 and GPT-4.
+
+In 2020, (Shreenabh Agrawal)[https://twitter.com/ShreenabhA] did the same with the previous GPT-3.
+
+And there are other users that did the same with that model.
+
+But now, with GPT-3.5, we can do more.
+
+
+""")
