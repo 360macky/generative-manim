cce5d6660d187615e99f1183d2a8fce59f830ac7
Marcelo Arias <hello@marceloarias.com>
2023-03-22 13:40:39 -0500

    Add prompt injection security for main.py

--- src/main.py
+++ src/main.py
@@ -12,11 +12,7 @@ st.set_page_config(
 st.title(":art: Generative Manim")
 st.write(":robot_face: Create beautiful and quick animations with GPT-3.5. :sparkles:")
 
-if 'is_code_generated' not in st.session_state:
-  st.session_state['is_code_generated'] = False
-  st.session_state['code_input'] = ""
-
-prompt = st.text_area("Write your animation idea here. Use simple words.", "Draw a blue circle and convert it to a red square")
+prompt = st.text_area("Write your animation idea here. Use simple words.", "Draw a blue circle and convert it to a red square", max_chars=240)
 
 if st.checkbox("Use own Open API Key (optional)"):
   openai_api_key = st.text_input("Paste your own [Open API Key](https://platform.openai.com/account/api-keys)", value="", type="password")
@@ -29,23 +25,40 @@ code_response = ""
 
 if generates_code:
 
+  if not openai_model:
+    openai_model = "gpt-4"
+
+  if not prompt:
+    st.error("Error: Please write a prompt to generate the video.")
+    st.stop()
+
+  # If prompt is less than 10 characters, it will be rejected
+  if len(prompt) < 10:
+    st.error("Error: Your prompt is too short. Please write a longer prompt.")
+    st.stop()
+
+  # If prompt exceeds 240 characters, it will be truncated
+  if len(prompt) > 240 and not openai_api_key:
+    st.error("Error: Your prompt is longer than 240 characters. Please shorten it, or use your own API key.")
+    st.stop()
+
+  # Prompt must be trimmed of spaces at the beginning and end
+  prompt = prompt.strip()
+
+  # Remove ", ', \ characters
+  prompt = prompt.replace('"', '')
+  prompt = prompt.replace("'", "")
+  prompt = prompt.replace("\\", "")
+
   # If user has their own API key, use it
   if not openai_api_key:
     openai.api_key = st.secrets["openai_api_key"]
   else:
     openai.api_key = openai_api_key
 
-  if not openai_model:
-    openai_model = "gpt-4"
-
-  # If prompt exceeds 240 characters, it will be truncated
-  if len(prompt) > 240:
-    st.error("Error: Your prompt is longer than 240 characters. Please shorten it, or use your own API key.")
-    st.stop()
-
   response = openai.ChatCompletion.create(
       model=openai_model.lower(),
-      messages=[{"role": "system", "content": "You write Manim scripts for animations in Python. Generate code, not text. Do not explain code. Do not add comments. Do not use other library than Manim. At the end use 'self.play' ```from manim import *\n\nclass GenScene(Scene):```\n  def construct(self):\n  # Write here"},
+      messages=[{"role": "system", "content": "Write Manim scripts for animations in Python. Generate code, not text. Do not explain code. Do not add comments. Do not use other library than Manim. At the end use 'self.play' ```from manim import *\n\nclass GenScene(Scene):```\n  def construct(self):\n  # Write here"},
                 {"role": "user", "content": f"New Animation Request: {prompt}"}],
       max_tokens=300
   )
@@ -53,7 +66,6 @@ if generates_code:
   code_response = extract_code(response.choices[0].message.content)
 
   code_response = remove_indentation(extract_construct_code(code_response))
-  st.session_state['is_code_generated'] = True
 
   if show_code:
     st.text_area(label="Code generated: ",
@@ -73,4 +85,4 @@ if generates_code:
     st.error("Error: Something went wrong processing your prompt. Please reload the page.")
 
 st.write('Made with :heart: by [Marcelo](https://github.com/360macky).')
-st.write('[Source code](https://github.com/360macky/generative-manim) - [Report a bug](https://github.com/360macky/generative-manim/issues/new) - [Twitter](https://twitter.com/360macky)')
+st.write('[Source code](https://github.com/360macky/generative-manim) - [Report a bug](https://github.com/360macky/generative-manim/issues/new) - [Twitter](https://twitter.com/360macky) - [OpenAI Profile](https://community.openai.com/u/360macky/summary)')
