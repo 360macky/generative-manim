6ec0bdef5bae34927d499dabef545ba15e125de4
Marcelo Arias <hello@marceloarias.com>
2023-03-22 13:41:09 -0500

    Move image of How it works

--- src/pages/3_⚡️_How_it_works.py
+++ src/pages/3_⚡️_How_it_works.py
@@ -8,31 +8,28 @@ st.write("## Concept")
 
 st.markdown("""
 
-Imagine a future where you can watch an animation video from a concept you have in your mind in a few seconds. That could be useful for teachers, students, and more. You won't need to learn how to use a video editor, how to draw, or how to code. You just need to write a text.
+Imagine a future where you can watch an animation video from a concept you have in your mind in a few seconds. That could be useful for teachers, students, and more. You won't need to learn how to use a video editor, how to draw, or how to code. You just need to write a descriptive text.
 
-That indeed is a future. But it's not that far. We can already start working on it. I have a feel that a pure AI editor like [Runway](https://runwayml.com), or an animation editor like [Jitter](https://jitter.video) could take advantage of a workflow like this.
-
-Storytelling, teaching, and creating it's about to change.
-
-
-
-
-The idea behind *Generative Manim* is test how far we can go with GPT-3.5 and GPT-4.
-
-In 2020, (Shreenabh Agrawal)[https://twitter.com/ShreenabhA] did the same with the previous GPT-3.
-
-And there are other users that did the same with that model.
-
-But now, with GPT-3.5, we can do more.
-
+That indeed is a future. But it's not that far. We can already start working on it. I have a feel that a pure AI editor like [Runway](https://runwayml.com), or an animation editor like [Jitter](https://jitter.video) could take advantage of a workflow like this:
 
 """)
 
-st.write(os.path.dirname(__file__) + "/blueprint.png")
-            
 blueprint = Image.open(os.path.dirname(__file__) + "/blueprint.png")
 
 st.image(blueprint, caption="Blueprint of Generative Manim", output_format="PNG")
+
+st.markdown("""
+
+Storytelling, teaching, and creating it's about to change with this.
+
+The idea behind *Generative Manim* is test how far we can go with GPT-3.5 and GPT-4.
+
+## Costs implications
+
+Currently GPT-4 is 30 times more expensive than GPT-3.5. That means that a 1000 tokens text will cost $30. That's a lot of money. But it's not that much if you think about the possibilities.
+
+""")
+
 
 st.write("## Acknowledgements")
 
