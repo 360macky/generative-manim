e3c047c47f8566d2f1370cc587ad94474f6e90ea
Marcelo Arias <hello@marceloarias.com>
2023-03-21 23:52:09 -0500

    Add new question to FAQ

--- src/pages/4_❓_FAQ.py
+++ src/pages/4_❓_FAQ.py
@@ -2,14 +2,19 @@ import streamlit as st
 
 st.title("❓ FAQ")
 
+st.write("## How the future could be?")
+
+st.write("Imagine a video editor that allows you to create animations just by writing a text. Manim could be that way the videos are created.")
+
 st.write("## What is Manim?")
 
-st.write("Manim is a Python library for creating mathematical animations. It's open source. The version used in this app is Manim Community, a fork of the original Manim library.")
+st.write("Manim is a Python library for creating mathematical animations. It's open source. The version used in this Streamlit app is Manim Community, a fork of the original Manim library.")
 
 st.write("## What is GPT-3.5?")
 
 st.write("GPT-3.5 is the latest API-available version of OpenAI's GPT model. It's a language model behind ChatGPT. GPT-4 itself is not available as an API.")
 
-st.write("## How the future could be?")
+st.write("## What is the purpose of this app?")
 
-st.write("Imagine a video editor that allows you to create animations just by writing a text. Manim could be that way the videos are created.")
+st.write("The purpose is to show how GPT-4 and GPT-3.5 can be used to export Manim code from a prompt. The design, which is simple, could be used to empower video editors, or apps in general.")
+
