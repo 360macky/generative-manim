b6d3fd491ee64c8f667e2266e63694a4ad2425bb
Marcelo Arias <hello@marceloarias.com>
2023-03-22 07:51:43 -0500

    Create Render Engine

--- src/pages/1_💻_Render_Engine.py
+++ src/pages/1_💻_Render_Engine.py
@@ -1,5 +1,26 @@
 import streamlit as st
+from manim import *
 
-st.markdown('# 💻 Render Engine')
+st.title('💻 Render Engine')
 
 st.write("Quick engine to render Manim code. Paste your code, and click generate. That's it!")
+
+code_response = st.text_area(label="Code generated: ",
+                             value="",
+                             key="code_input")
+
+generates_code = st.button(
+    ":computer: Render video :sparkles:", type="primary")
+
+if generates_code:
+  class RenderedScene(Scene):
+    def construct(self):
+      exec(code_response)
+
+  RenderedScene().render()
+  try:
+    st.video("media/videos/1080p60.0/RenderedScene.mp4")
+  except FileNotFoundError:
+    st.error("Error: I couldn't find the generated video file. I know this is a bug and I'm working on it. Please reload the page.")
+  except:
+    st.error("Error: Something went wrong processing your prompt. Please reload the page.")
