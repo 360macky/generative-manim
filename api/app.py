1d55dfca570ad9981f7f52af0b6208c2cee1b47f
Marcelo Arias <hello@marceloarias.com>
2024-04-16 22:36:52 -0500

    Add /zero-shot-learning API endpoint

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from subprocess import run, PIPE, Popen, CalledProcessError
 import threading
+from openai import OpenAI
 
 load_dotenv()
 app = Flask(__name__)
@@ -13,10 +14,31 @@ def hello_world():
     return "GM API"
 
 
-@app.route("/text-to-code", methods=["POST"])
-def text_to_code():
-    # TODO: To be implemented
-    return jsonify({"message": "Not implemented yet"}), 501
+@app.route("/zero-shot-learning", methods=["POST"])
+def zero_shot_learning():
+    text = request.json.get("text")
+    prompt = f"Generate python code for the following text:\n\n{text}"
+    client = OpenAI()
+    GPT_SYSTEM_INSTRUCTIONS = """Write Manim scripts for animations in Python. Generate code, not text. Never explain code. Never add functions. Never add comments. Never infinte loops. Never use other library than Manim/math. Only complete the code block. Use variables with length of maximum 2 characters. At the end use 'self.play'.
+
+```
+from manim import *
+from math import *
+
+class GenScene(Scene):
+    def construct(self):
+        # Write here
+```"""
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": GPT_SYSTEM_INSTRUCTIONS},
+            {"role": "user", "content": prompt},
+        ],
+    )
+    
+    code = response.choices[0].message
+    return jsonify({"code": code}), 200
 
 
 @app.route("/code-to-video", methods=["POST"])
