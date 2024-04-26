7dfbb7d7aae63646273874edfd55c89c91374deb
Marcelo Arias <hello@marceloarias.com>
2024-04-25 20:56:36 -0500

    Remove GenScene.mp4 and GenScene.py as files from .gitignore

---
+++ GenScene.py
@@ -0,0 +1,9 @@
+# Manim code generated with OpenAI GPT
+# Command to generate animation: manim GenScene.py GenScene --format=mp4 --media_dir . --custom_folders video_dir
+
+from manim import *
+from math import *
+
+class GenScene(Scene):
+    def construct(self):
+        pass
